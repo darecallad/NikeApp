b02e4720e712b7ca071b8551e3ac0e03f63fb35d
Johnson <Ashane199080706@hotmail.com.tw>
2023-12-27 15:15:28 -0800

    Subscribe section done.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
@@ -15,7 +16,7 @@ const Button = ({
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : `bg-coral-red  text-white border-coral-red`
       }
-      rounded-full`}
+      rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
       {iconURL && (
